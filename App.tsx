95a10284eec22dff35517858c89ef5a373cd151d
Hamon Giacomelli <hamongtgiacomelli@gmail.com>
2021-06-06 10:18:26 -0300

    remove settings page

--- App.tsx
+++ App.tsx
@@ -1,15 +1,12 @@
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
 import React from "react";
-import { Image } from "react-native";
-import { TouchableOpacity } from "react-native-gesture-handler";
 import { Provider } from "react-redux";
 import { createStore } from "redux";
 import CharacterPage from "./src/page/CharacterPage";
 import HomePage from "./src/page/HomePage";
 import LoginPage from "./src/page/LoginPage";
 import RegisterPage from "./src/page/RegisterPage";
-import SettingsPage from "./src/page/SettingsPage";
 import TablePage from "./src/page/TablePage";
 import reducers from "./src/reducer";
 import storage from "redux-persist/lib/storage";
@@ -26,7 +23,6 @@ const store = createStore(persistedReducer);
 const persistor = persistStore(store);
 
 const Stack = createStackNavigator();
-const image = require("./assets/settings_icon.png");
 
 export default function App() {
   return (
@@ -37,7 +33,6 @@ export default function App() {
             <Stack.Screen name="Login" component={LoginPage} />
             <Stack.Screen name="Register" component={RegisterPage} />
             <Stack.Screen name="Home" component={HomePage} />
-            <Stack.Screen name="Settings" component={SettingsPage} />
             <Stack.Screen name="Character" component={CharacterPage} />
             <Stack.Screen name="Table" component={TablePage} />
           </Stack.Navigator>
